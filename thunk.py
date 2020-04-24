18e14d6af4da374635c663b8a395715d66b0cc93
Andrew Lamoureux <andrew@vector35.com>
2020-04-24 00:28:48 -0400

    Wrap dll call in a thread

--- thunk.py
+++ thunk.py
@@ -5,22 +5,34 @@
 import os
 import binascii
 import platform
+import threading
 
 from ctypes import *
 
-def doit(shellcode):
+def call_dll(dll, shellcode):
+	print('THUNK: calling to dll')
+	rc = dll.doit(c_char_p(shellcode), len(shellcode))
+	print('THUNK: dll returned %d' % rc)
+
+def doit(shellcode, use_thread=True):
 	shellcode_str = binascii.hexlify(shellcode).decode('utf-8')
 	print('THUNK: running', shellcode_str)
-	
+
+	# resolve path to dll
 	fpath = os.path.abspath(__file__)
 	fpath = os.path.dirname(fpath)
 	if platform.system() == 'Darwin':
 		fpath = os.path.join(fpath, 'callbuf.dylib')
 
+	# load dll
 	print('THUNK: loading', fpath)
 	dll = CDLL(fpath)
 
-	print('THUNK: calling')
-	rc = dll.doit(c_char_p(shellcode), len(shellcode))
+	# call into dll
+	if use_thread:
+		print('THUNK: creating thread')
+		threading.Thread(target=call_dll, args=(dll, shellcode)).start()
+	else:
+		call_dll(shellcode)
 
-	print('THUNK: returned %d' % rc)
+	print('THUNK: returning')
